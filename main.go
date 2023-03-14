442877388fdc3845f392dd262b9ea4cb3df843e1
Ben Kraft <ben@benkraft.org>
2023-03-13 20:59:24 -0700

    starting to poke at diff

--- main.go
+++ main.go
@@ -7,11 +7,7 @@ import (
 	"github.com/go-git/go-git/v5"
 )
 
-func head(args ...string) error {
-	r, err := git.PlainOpen(".")
-	if err != nil {
-		return err
-	}
+func head(r *git.Repository, args ...string) error {
 	ref, err := r.Head()
 	if err != nil {
 		return err
@@ -20,11 +16,45 @@ func head(args ...string) error {
 	return nil
 }
 
-func diff(args ...string) error {
+func diff(r *git.Repository, args ...string) error {
+	head, err := r.Head()
+	if err != nil {
+		return err
+	}
+	headCommit, err := r.CommitObject(head.Hash())
+	if err != nil {
+		return err
+	}
+	switch headCommit.NumParents() {
+	case 0:
+		return fmt.Errorf("can't diff on initial commit")
+	case 1:
+	default:
+		return fmt.Errorf("can't diff on merge commit")
+	}
+
+	parent, err := headCommit.Parent(0)
+	if err != nil {
+		return err
+	}
+
+	mainBranch, err := r.Branch("main")
+	if err != nil {
+		return err
+	}
+	main, err := r.Reference(mainBranch.Merge, true)
+	if err != nil {
+		return err
+	}
+	if parent.Hash != main.Hash() {
+		return fmt.Errorf("TODO: must be one commit ahead of main for now, but parent was %v and main is %v", parent.Hash, main.Hash())
+	}
+	fmt.Println(parent, main)
+
 	return nil
 }
 
-var commands = map[string]func(...string) error{
+var commands = map[string]func(*git.Repository, ...string) error{
 	"diff": diff,
 	"head": head,
 }
@@ -37,7 +67,13 @@ func Main() {
 		os.Exit(1)
 	}
 
-	err := command(os.Args[2:]...)
+	r, err := git.PlainOpen(".")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = command(r, os.Args[2:]...)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
