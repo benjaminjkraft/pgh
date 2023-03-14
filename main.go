a3597276a23e6d8a845954c36633cd61e932333c
Ben Kraft <ben@benkraft.org>
2023-03-14 08:36:02 -0700

    first pass at testing in-memory

--- main.go
+++ main.go
@@ -2,50 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/go-git/go-git/v5"
 )
 
-func diff(r *git.Repository, args ...string) error {
-	head, err := r.Head()
-	if err != nil {
-		return err
-	}
-	headCommit, err := r.CommitObject(head.Hash())
-	if err != nil {
-		return err
-	}
-	switch headCommit.NumParents() {
-	case 0:
-		return fmt.Errorf("can't diff on initial commit")
-	case 1:
-	default:
-		return fmt.Errorf("can't diff on merge commit")
-	}
-
-	parent, err := headCommit.Parent(0)
-	if err != nil {
-		return err
-	}
-
-	mainBranch, err := r.Branch("main")
-	if err != nil {
-		return err
-	}
-	main, err := r.Reference(mainBranch.Merge, true)
-	if err != nil {
-		return err
-	}
-	if parent.Hash != main.Hash() {
-		return fmt.Errorf("TODO: must be one commit ahead of main for now, but parent was %v and main is %v", parent.Hash, main.Hash())
-	}
-	fmt.Println(parent, main)
-
-	return nil
+type runner struct {
+	repo *git.Repository
+	out  io.Writer
 }
 
-var commands = map[string]func(*git.Repository, ...string) error{
+var commands = map[string]func(*runner, ...string) error{
 	"diff": diff,
 }
 
@@ -63,7 +31,7 @@ func Main() {
 		os.Exit(1)
 	}
 
-	err = command(r, os.Args[2:]...)
+	err = command(&runner{r, os.Stdout}, os.Args[2:]...)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
