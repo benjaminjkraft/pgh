31353b7aa5b4596d81e2e4690eddd6de95a4cf0d
Ben Kraft <ben@benkraft.org>
2023-04-21 20:17:58 -0700

    just shell out for tests

--- diff_test.go
+++ diff_test.go
@@ -2,64 +2,33 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
-	"path/filepath"
+	"os/exec"
 	"strings"
 	"testing"
-	"time"
 
 	git "github.com/libgit2/git2go/v28"
 )
 
-func commit(repo *git.Repository, message string, filenames ...string) error {
-	loc, err := time.LoadLocation("America/Buenos_Aires")
-	if err != nil {
-		return err
-	}
-
-	sig := &git.Signature{
-		Name:  "Jorge Luis Borges",
-		Email: "borges@example.com",
-		When:  time.Date(1986, time.June, 14, 0, 0, 0, 0, loc),
-	}
-
-	index, err := repo.Index()
-	if err != nil {
-		return err
-	}
-	for _, filename := range filenames {
-		err = index.AddByPath(filename)
+func runCommands(cwd string, commands string) error {
+	for _, line := range strings.Split(commands, "\n") {
+		cleaned := strings.TrimSpace(line)
+		if cleaned == "" {
+			continue
+		}
+		cmd := &exec.Cmd{
+			Path:   "/bin/sh",
+			Args:   []string{"sh", "-c", cleaned},
+			Dir:    cwd,
+			Stdout: os.Stdout,
+			Stderr: os.Stderr,
+		}
+		err := cmd.Run()
 		if err != nil {
-			return err
+			return fmt.Errorf("cmd `%v` failed: %w", cleaned, err)
 		}
 	}
-	err = index.Write()
-	if err != nil {
-		return err
-	}
-	treeID, err := index.WriteTree()
-	if err != nil {
-		return err
-	}
-	tree, err := repo.LookupTree(treeID)
-	if err != nil {
-		return err
-	}
-	var parents []*git.Commit
-	headID, err := referenceNameToID(repo, "HEAD")
-	if err == nil {
-		headCommit, err := repo.LookupCommit(headID)
-		if err != nil {
-			return err
-		}
-		parents = append(parents, headCommit)
-	} else if !git.IsErrorCode(err, git.ErrorCodeNotFound) {
-		return err
-	}
-
-	_, err = repo.CreateCommit("HEAD", sig, sig, message, tree, parents...)
-	return err
+	return nil
 }
 
 func makeTestRepo() (repo *git.Repository, cleanup func(), err error) {
@@ -74,59 +43,23 @@ func makeTestRepo() (repo *git.Repository, cleanup func(), err error) {
 		}
 		os.RemoveAll(tmpdir)
 	}
-	// TODO(benkraft): Might actually be better to rewrite all this to just
-	// shell out to make it easy to understand what we are making.
-	repo, err = git.InitRepository(tmpdir, false)
+
+	err = runCommands(tmpdir, `
+		git init
+		echo content >content
+		git add content
+		git commit -am "Initial commit"
+		git branch -M main
+		git checkout -b mybranch
+		echo updated content >content
+		git commit -am "Another commit"
+	`)
 	if err != nil {
 		return nil, cleanup, err
 	}
 
-	filename := "content"
-	err = ioutil.WriteFile(filepath.Join(tmpdir, filename), []byte("content\n"), 0o644)
-	if err != nil {
-		return nil, cleanup, err
-	}
-	err = commit(repo, "Initial commit\n", filename)
-	if err != nil {
-		return nil, cleanup, err
-	}
-
-	mainRef, err := repo.References.Lookup("refs/heads/master")
-	if err != nil {
-		return nil, cleanup, err
-	}
-	_, err = mainRef.Rename("refs/heads/main", false, "rename master -> main")
-	if err != nil {
-		return nil, cleanup, err
-	}
-	mainCommit, err := repo.LookupCommit(mainRef.Target())
-	if err != nil {
-		return nil, cleanup, err
-	}
-
-	branch, err := repo.CreateBranch("mybranch", mainCommit, false)
-	if err != nil {
-		return nil, cleanup, err
-	}
-
-	headRef, err := repo.References.Lookup("HEAD")
-	if err != nil {
-		return nil, cleanup, err
-	}
-	_, err = headRef.SetSymbolicTarget(branch.Reference.Name(), "checkout mybranch")
-	if err != nil {
-		return nil, cleanup, err
-	}
-
-	err = ioutil.WriteFile(filepath.Join(tmpdir, filename), []byte("updated content\n"), 0o644)
-	if err != nil {
-		return nil, cleanup, err
-	}
-	err = commit(repo, "Another commit\n", filename)
-	if err != nil {
-		return nil, cleanup, err
-	}
-	return repo, cleanup, nil
+	repo, err = git.OpenRepository(tmpdir)
+	return repo, cleanup, err
 }
 
 func TestDiffSmoke(t *testing.T) {
